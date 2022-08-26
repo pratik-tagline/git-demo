cc97828296dce51f81beeb65b20e5ff7432d54de
pratik-tagline <pratikm.tagline@gmail.com>
2022-08-26 15:11:08 +0530

    "Modified m1.ts & m2.ts file in develop branch"

--- m1.ts
+++ m1.ts
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 import { rebase } from "./rebase";
 
 const person1 = new rebase(1, "xyx", "good");
 
 console.log(person1.name);
 console.log(person1.getDetail());
+=======
+class Employee {
+  private empCode: number;
+  empName: string;
+}
+
+let emp = new Employee();
+// emp.empCode = 123; // Compiler Error
+emp.empName = "Swati"; //OK
+>>>>>>> f89c689 (Modified m1.ts & m2.ts file in develop branch)
--- m2.ts
+++ m2.ts
@@ -1,2 +1,26 @@
+<<<<<<< HEAD
 let hello = prompt;
 let goodBye = hello;
+=======
+class employee {
+  public empName: string;
+  protected empCode: number;
+
+  constructor(name: string, code: number) {
+    this.empName = name;
+    this.empCode = code;
+  }
+}
+
+class SalesEmployee extends Employee {
+  private department: string;
+
+  constructor(name: string, code: number, department: string) {
+    super();
+    this.department = department;
+  }
+}
+
+let empl = new SalesEmployee("John Smith", 123, "Sales");
+// emp.empCode; //Compiler Error
+>>>>>>> f89c689 (Modified m1.ts & m2.ts file in develop branch)
